defcb4b9dbf82598d8c8fac0e3afda3c108e6dd1
jvujcic <jvujcic@gmail.com>
2015-03-05 14:53:41 +0100

    .

---
+++ Cetvrtak/Vjezbe01/zad02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+struct Tocka {
+    int x, y;
+};
+
+// Napisite funkcije za udaljenost i centralnu simetriju
+
+int main() {
+    Tocka A, B;
+    cout << "Ucitaj koordinate 1. tocke:" << endl;
+    cin >> A.x >> A.y;
+    cout << "Ucitaj koordinate 2. tocke:" << endl;
+    cin >> B.x >> B.y;   
+    return 0;
+}
