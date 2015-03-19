d27039b690e6bff3da586f04fc9e56a50818b9ea
jvujcic <jvujcic@gmail.com>
2015-03-19 13:33:32 +0100

    .

--- Cetvrtak/Vjezbe02/zad03.cpp
+++ Cetvrtak/Vjezbe02/zad03.cpp
@@ -6,15 +6,26 @@ struct tocka {
     int x, y;
 
     tocka() {   //postavlja u ishodiste
-    }
+       x = 0;
+       y = 0; 
+       cout << "Kreirana tocka " << x << ", "  << y << endl;
+   }
 
     tocka(int a, int b) {
+        x = a;
+        y = b;
+        cout << "Kreirana tocka " << x << ", "  << y << endl;
     }
 
     tocka simetricna() {
+        tocka sim(-x, -y);
+        // sim.x = -x;
+        // sim.y = -y;
+        return sim;
     } 
 
     ~tocka() {  //ispisuje koordinate tocke
+        cout << "Unistena tocka " << x << ", "  << y << endl;
     }   
 };
 
