5beda5c558ff32934b237e842289dda5fb4580e8
jvujcic <jvujcic@gmail.com>
2015-05-06 13:23:33 +0200

    .

--- Srijeda/Vjezbe07/zad01.cpp
+++ Srijeda/Vjezbe07/zad01.cpp
@@ -14,28 +14,28 @@ struct list
     {
         Type *gdje;
         
-        iterator (Type *ptr) { gdje = ptr; }
-
         Type data() { return *gdje; }
-        //next
-        //isEqual
+        iterator next() { return iterator(gdje+1); }
+        int isEqual ( iterator it ) { if (gdje == it.gdje) return 1; else return 0; }
+        
+        iterator (Type *ptr) { gdje = ptr; }
     };
     
     iterator begin() { iterator rez(&podaci[0]); return rez; }
-    //end
+    iterator end() { return iterator(&podaci[duljina]); }
 };
 
 int main ( void )
 {
-    // list<int> L;
-    // L.push_back(5); L.push_back(7);
+    list<int> L;
+    L.push_back(5); L.push_back(7);
 
-    // list<int>::iterator li = L.begin();
-    // while (!li.isEqual(L.end()))
-    // {
-    //     cout << li.data() << " ";
-    //     li = li.next();
-    // }
+    list<int>::iterator li = L.begin();
+    while (!li.isEqual(L.end()))
+    {
+        cout << li.data() << " ";
+        li = li.next();
+    }
 
     return 0;
 } 
